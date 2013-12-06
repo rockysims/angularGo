93227165c13bf469d3d394cb7af793bfb30ef356
Rocky Sims <Rocky.Sims@gmail.com>
2013-12-06 01:49:49 -0800

    bug where board disappears when board[i][j] = i*j; is changed to board[i][j] = 33; in goBoard control

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -4,5 +4,24 @@
 
 var myGoApp = angular.module('myGoApp', []);
 myGoApp.controller('goBoard', function($scope) {
-	$scope.test = "aweofijaweofij";
+	var EMPTY = 0;
+	var BLACK = 1;
+	var WHITE = 2;
+	
+	var board = [];
+	var size = 9;
+	
+	for (var i = 0; i < size; i++) {
+		board[i] = [];
+		for (var j = 0; j < size; j++) {
+			board[i][j] = i*j;
+			//board[i][j] = 33; //TODO: uncomment out this line to reproduce bug where the whole board table disappears
+			board[0][2] = 98765;
+		}
+	}
+	
+	
+	$scope.c = $scope.c++ || 1;
+	
+	$scope.board = board;
 });
